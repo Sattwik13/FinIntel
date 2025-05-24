ca6a4bd8d7f5abf0902a9b640a832f3e1906ab17
Sattwik13 <sattwikmanna6359@gmail.com>
2025-05-24 16:23:05 +0530

    Create a database model in supabase Using Prisma

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,8 +5,8 @@ import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
 
 
-
-const Header = () => {
+const Header = () => { 
+  
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
 
