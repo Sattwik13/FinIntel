e25af309bf073a870f06120fa432e4b3b01b6798
Sattwik13 <sattwikmanna6359@gmail.com>
2025-05-29 23:19:50 +0530

    domain related issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "randomuser.me",
-                pathname: '/photos/**',
-            }
-        ]
-    },
+    domains: ['randomuser.me']
+  },
 
     experimental: {
         serverActions: {
