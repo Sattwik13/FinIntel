08ef4c5a03ad670b032b81d4bfbe2f63af248291
Sattwik13 <sattwikmanna6359@gmail.com>
2025-05-29 23:08:14 +0530

    issue fixed

--- src/app/page.js
+++ src/app/page.js
@@ -68,7 +68,7 @@ export default function Home() {
      {/* Testimonials Section */}
       <section className="py-20 ">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-12">
+          <h2 className="text-3xl font-bold text-center mb-12 text-emerald-800">
             What Our Users Say
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
